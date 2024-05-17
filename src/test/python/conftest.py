ae079c15c7339bf42af78961230b8fb867e02562
SAMAX143 <samax143@kdrp.com>
2024-05-17 03:04:50 -0700

    tests

--- src/test/python/conftest.py
+++ src/test/python/conftest.py
@@ -5,10 +5,10 @@ from appium import webdriver
 def driver():
     desired_caps = {
         "platformName": "Android",
-        "deviceName": "Pixel 3a API 34",
-        "app": "src/test/appium/score.apk",
+        "deviceName": "emulator-5554",
+        "app": r"C:\Users\grand\thescore-appium-tests\src\test\appium\score.apk",
         "automationName": "UiAutomator2"
     }
-    driver = webdriver.Remote("http://127.0.0.1:4723/", desired_caps)
+    driver = webdriver.Remote("http://localhost:4723", desired_caps)
     yield driver
     driver.quit()
